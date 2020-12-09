d0bd7fdd28ff1e600dcb5f6da8a11fe992bd13a2
Amio Rahman <hm.amio.rahman@gmail.com>
2020-12-09 23:56:38 +0100

    SQLIte Connection

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Portfolio
 
 # Create your views here.
+
+def home(request):
+    portfolio = Portfolio.objects.all()
+    return render(request, 'portfolios/home.html', {'projects': portfolio})
