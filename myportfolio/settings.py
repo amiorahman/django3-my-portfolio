70dea1ff63dc3641f6b20d626e2231709cba7599
Amio Rahman <madapaga@iut-dhaka.edu>
2020-12-15 18:41:19 +0000

    gitignore added and cache deleted

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')vk!*vbwc#8#o4dfdx1*fk82-9gen1k=*)!yp+@_+g-u*56pv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amiorahman.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
